4f85a8abc8d7c61a6112c589fb63fa2d16ba3a64
gagichce <gagichce@mcmaster.ca>
2015-12-06 15:36:15 -0500

    init commit - blank project

---
+++ Promistr/Promistr/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Promistr.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Promistr/Promistr/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Promistr.Startup))]
+namespace Promistr
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
